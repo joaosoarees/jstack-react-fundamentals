746ebe7ffb8a39ebda8edbbac169f908c33b8530
João Vitor <jsoareez@gmail.com>
2021-07-24 13:56:01 -0300

    jsx fundamentals

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-const user = {};
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(user?.address?.street)
+ReactDOM.render(
+  <h1>Olá Mundo</h1>,
+  document.getElementById('root'),
+)

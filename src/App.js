e6450e68246a7bf5dd5feeb0f356ff0ac1fd26c0
João Vitor <jsoareez@gmail.com>
2021-07-24 16:28:11 -0300

    react states

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from './Header';
 import { Post } from './Post';
 
-const posts = [
-  {
-    title: 'Title#01',
-    subTitle: 'Subtitle#01',
-    likes: 15,
-  },
-  {
-    title: 'Title#01',
-    subTitle: 'Subtitle#01',
-    likes: 20,
-  },
-  {
-    title: 'Title#01',
-    subTitle: 'Subtitle#01',
-    likes: 30,
-  },
-];
-
 export function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Title#02', subTitle: 'Subtitle#02', likes: 20 },
+    { id: 2, title: 'Title#01', subTitle: 'Subtitle#01', likes: 15 },
+  ]);
+
+  console.log({posts})
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: prevState.length + 1,
+          title: `Title#0${prevState.length + 1}`,
+          subTitle: `Subtitle#0${prevState.length + 1}`,
+          likes: 50
+        }
+      ])
+    }, 2000);
+  }
+
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
       {posts.map(post => (
         <Post
-          key={Math.random()}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,

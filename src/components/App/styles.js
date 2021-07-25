6ab20a91a38c907c4e91aed37f8356737a2d0513
João Vitor <jsoareez@gmail.com>
2021-07-25 16:16:37 -0300

    refact project

--- src/components/App/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-import * as S from '../Title';
-
-export const Title = styled(S.Title)`
-  color: red;
-`;
--- src/components/Title.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-  color: #637BF3;
-  font-family: 'Roboto', sans-serif;
-`;

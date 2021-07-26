bb93e17a248d65619ed06253b628b64e3bdd3615
João Vitor <jsoareez@gmail.com>
2021-07-25 23:13:35 -0300

    typing propTypes in class Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 export default class Header extends React.Component {
+  static propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired
+  };
+
   render () {
     const { onToggleTheme, selectedTheme } = this.props;
 

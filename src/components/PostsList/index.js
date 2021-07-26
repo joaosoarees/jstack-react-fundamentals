67991deb387345ff5c678d3b82cc127ccc5a6594
João Vitor <jsoareez@gmail.com>
2021-07-25 22:02:52 -0300

    convert PostsList to class component

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -4,17 +4,18 @@ import { Container } from './styles';
 import Post from './Post';
 
 import posts from './posts';
-
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
-}
+export default class PostsList extends React.Component {
+  render () {
+    return (
+      <Container>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </Container>
+    );
+  };
+};
